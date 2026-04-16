c696ee149ab4dbb7d311fa8bbbf8edd9621587fc
jty0221-del <jty0221@gmail.com>
2026-04-16 19:28:04 +0900

    fix: dashboard syntax error at line 760 - dateStr split

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -757,7 +757,8 @@ export default function Dashboard() {
 
   const totalTodo = todoList.reduce((s, t) => s + t.count, 0)
   const today = new Date()
-  const dateStr = today.getFullYear() + '년 ' + (today.getMonth() + 1) + '월 ' + today.getDate() + '일 ' + ['일','월','화','수','목','금','토'][today.getDay()] + '요일'
+  const dayNames = ['일','월','화','수','목','금','토']
+  const dateStr = today.getFullYear() + '년 ' + (today.getMonth() + 1) + '월 ' + today.getDate() + '일 ' + dayNames[today.getDay()] + '요일'
 
   return (
     <div className="flex min-h-screen bg-[#F2F4F6]">
@@ -1378,5 +1379,6 @@ export default function Dashboard() {
       )}
     </div>
   )
+}
 }
 }
