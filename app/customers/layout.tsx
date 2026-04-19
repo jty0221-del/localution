cf5971b3232d2aa240c4af8a1ca0732761cf238a
jty0221-del <jty0221@gmail.com>
2026-04-20 00:01:22 +0900

    feat(entitlements): Phase0.5 #3 customers layout 가드 (crm)

---
+++ app/customers/layout.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+/**
+ * /customers 전체 권한 가드
+ * 요구 모듈: crm
+ */
+
+import GatedRoute from '../components/GatedRoute'
+
+export default function CustomersLayout({ children }: { children: React.ReactNode }) {
+  return <GatedRoute moduleId="crm">{children}</GatedRoute>
+}
